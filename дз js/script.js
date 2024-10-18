92dbb12f6857d891eab19c19756f8421e57ca3fb
000Kissel <147407107+000Kissel@users.noreply.github.com>
2024-10-18 15:24:43 +0500

    Add files via upload

---
+++ дз js/script.js
@@ -0,0 +1,57 @@
+// 1
+
+let test = 5;
+let res;
+res = test > 10 ? 'Верно' : 'Неверно';
+console.log(res);
+
+// 2
+
+let test1 = 8;
+let res1;
+res1 = test1 < 10 ? 'Верно' : 'Неверно';
+console.log(res1);
+
+// 3
+
+let test2 = 12;
+let res2;
+res2 = test2 >= 10 ? 'Верно' : 'Неверно';
+console.log(res2);
+
+// 4
+
+let str = 34;
+let luc;
+luc = str <= 10 ? 'Верно' : 'Неверно';
+console.log(luc);
+
+// 5
+
+let est = 10;
+let rts;
+rts = est == 10 ? 'Верно' : 'Неверно';
+console.log(rts);
+
+// 6
+
+let ste = 185;
+let rst;
+rst = ste != 10 ? 'Верно' : 'Неверно';
+console.log(rst);
+
+// 7
+
+let test3 = 45;
+let test4 = 11;
+let ost;
+ost = test3 > test4 ? 'Верно' : 'Неверно';
+console.log(ost);
+
+// 8
+
+let test5 = 5;
+let test6 = 5;
+let ost1;
+ost1 = test5 == test6 ? 'Верно' : 'Неверно';
+console.log(ost1);
